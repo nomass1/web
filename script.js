66b07fd81f883a416aee33ead327e51961c41021
nomass1 <watcharawit9669@gmail.com>
2024-11-28 23:29:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// กำหนดคะแนนใน JavaScript
+const studentScore = 1; // เปลี่ยนตัวเลขนี้ได้ตามต้องการ
+
+// อัปเดตคะแนนบนหน้าเว็บ
+document.getElementById('score-display').textContent = studentScore;
